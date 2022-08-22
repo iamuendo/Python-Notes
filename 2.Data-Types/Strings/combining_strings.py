c22c2c429465440a8de0ea4a7a191c88094fad7a
iamuendo <iamuendo@gmail.com>
2022-08-22 12:01:20 +0300

    combining strings

---
+++ 2.Data-Types/Strings/combining_strings.py
@@ -0,0 +1,9 @@
+# The method for combining strings is known as concatenation.
+first_name = "Isaac"
+last_name = "\tMuendo"
+
+full_name = first_name + "" + last_name
+
+# Display full name
+message = "Hello, my name is " + full_name
+print(message)

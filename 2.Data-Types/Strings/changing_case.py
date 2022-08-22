9246dcaffb9b93a99a10bd55ea89d42bb010aa83
iamuendo <iamuendo@gmail.com>
2022-08-22 12:18:00 +0300

    String cases

---
+++ 2.Data-Types/Strings/changing_case.py
@@ -0,0 +1,12 @@
+name = "isaac muendo"
+message = "Hello, my name is "
+
+# Using the title() method: Titlecase
+# Titlecase is where each word begins with a capital letter
+print("Title case: " + message + name.title())
+
+# Using the upper() method: Uppercase
+print("Upper case: " + message + name.upper())
+
+# Using the lower() method: Lowercase
+print("Lower case: " + message + name.lower())

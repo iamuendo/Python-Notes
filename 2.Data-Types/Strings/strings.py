40428310a3c02d020b32ae0601ccd90c3bda5cb1
iamuendo <iamuendo@gmail.com>
2022-08-22 11:10:31 +0300

    Data Types

---
+++ 2.Data-Types/Strings/strings.py
@@ -0,0 +1,13 @@
+# Single & double quotations can be used
+
+'''Single quotation
+   - Allows you to use double quoted inside a string
+'''
+favourite_quote = '"If you talk to a man in a language he understands, that goes to his head. If you talk to a man in his own language, that goes to his heart."- Nelson Mandela'
+print(favourite_quote)
+
+'''Double quotation
+   - Allows you to use single quotes inside a string
+'''
+favourite_novel = "Edward Snowden's, Permanent Record\n"
+print(favourite_novel)

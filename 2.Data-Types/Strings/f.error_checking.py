b4e46810747253439500174e2541188ab086a280
iamuendo <iamuendo@gmail.com>
2022-08-23 11:00:44 +0300

    Error Checking

--- 2.Data-Types/Strings/f.error_checking.py
+++ 2.Data-Types/Strings/f.error_checking.py
@@ -2,5 +2,5 @@
 # Occur where the python compiler doesn't recognise a section of your program as valid python code
 
 # For example: when you use an apostrophe within single quotation marks.
-message = 'That is a lion's den.'
-print(message)
+sentence = 'That is a lion's den.'
+print(sentence)

df3b459a6f38813b02cef95a919f26e3223bf04f
iamuendo <iamuendo@gmail.com>
2022-08-23 10:57:02 +0300

    Alphabetical arrangement

---
+++ 2.Data-Types/Strings/f.error_checking.py
@@ -0,0 +1,6 @@
+# Syntax errors are the most common errors
+# Occur where the python compiler doesn't recognise a section of your program as valid python code
+
+# For example: when you use an apostrophe within single quotation marks.
+message = 'That is a lion's den.'
+print(message)

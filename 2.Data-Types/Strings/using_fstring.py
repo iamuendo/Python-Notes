72f920ab288e5f3886b8621a694847d8a50e5570
iamuendo <iamuendo@gmail.com>
2022-08-23 10:43:37 +0300

    Formated strings

---
+++ 2.Data-Types/Strings/using_fstring.py
@@ -0,0 +1,6 @@
+# f-string - formated strings
+# It provides an easy way to embed python expressions inside string literals for formatting.
+# Use curly brackets to embed expressions
+name = 'Isaac Muendo'
+age = 22
+print(f"Hello, my name is {name} and I'm {age} years old.")
